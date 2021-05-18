e74570f82cf48fb79958ca714b8a3943a17c8ef2
rachelxneumann <41024651+rachelxneumann@users.noreply.github.com>
2021-05-18 12:27:09 -0700

    top scroll java script

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-THIS IS ONLY A SAMPLE
+function topFunction() {
+  document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+
+window.onscroll = function() {hideFunction()};
+
+function hideFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100)
+    document.getElementById("topBtn").style.display = "block";
+  else {
+    document.getElementById("topBtn").style.display = "none";
+  }
+}
+
